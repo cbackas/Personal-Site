a544be7fc56b81e3121b40d3851376316d33d011
Zac Gibson <zgibson@live.com>
2020-05-29 14:34:26 -0500

    Moved CSS around and got basic page format up on its feet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  myStyle: object = {};
   myParams: object = {};
   width: number = 100;
   height: number = 100;
 
   ngOnInit() {
-    this.myStyle = {
-      position: 'fixed',
-      width: '100%',
-      height: '100%',
-      'z-index': -1,
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      'background-color': 'rgb(25, 27, 28)',
-    };
-
     this.myParams = {
       particles: {
         number: {
