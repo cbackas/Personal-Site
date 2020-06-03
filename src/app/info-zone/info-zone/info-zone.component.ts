5f81dacbcfa614f0c8c041b399715bd25fe5f648
Zac Gibson <zgibson@live.com>
2020-06-03 18:00:47 -0500

    scss variable stuff and trying out grid in an info area component

--- src/app/info-zone/info-zone/info-zone.component.ts
+++ src/app/info-zone/info-zone/info-zone.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-info-zone',
   templateUrl: './info-zone.component.html',
-  styleUrls: ['./info-zone.component.scss']
+  styleUrls: ['./info-zone.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class InfoZoneComponent implements OnInit {
 
