1d39246013a0a973084929472df8584655ef0ad4
Zac Gibson <zgibson@live.com>
2020-06-03 02:02:12 -0500

    trying some stuff to get color responsive dividers

--- src/app/info-zone/selection/selection.component.ts
+++ src/app/info-zone/selection/selection.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-selection',
   templateUrl: './selection.component.html',
-  styleUrls: ['./selection.component.scss']
+  styleUrls: ['./selection.component.scss'],
 })
 export class SelectionComponent implements OnInit {
   links: Array<String> = ['languages', 'tools', 'projects'];
 
-  ngOnInit(): void {
+  style: Object = {
+
+  };
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      const url = event instanceof NavigationEnd ? event.url : null;
+      console.log(url);
+    });
   }
 
+  ngOnInit(): void {}
 }
