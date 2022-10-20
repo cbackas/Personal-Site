538dd738c5ac3f0734f357c0acb7e4f1a0b05879
cbackas <zgibson@live.com>
2022-10-19 21:24:06 -0500

    use a skew instead of a polygon to make angled line

--- components/TitleArea/TitleArea.tsx
+++ components/TitleArea/TitleArea.tsx
@@ -1,9 +1,10 @@
-import { Icon, VStack, Box, Center } from '@chakra-ui/react'
+import { VStack, Box, Center } from '@chakra-ui/react'
 import ButtonCard from './ButtonCard'
 import TitleCard from './TitleCard'
 
 const TitleArea = (): JSX.Element => {
   const bg = '#181a1b'
+  const skew = 3
 
   return (
     <Box maxW='100%' mx='auto' h='100vh' zIndex='2'>
@@ -16,29 +17,11 @@ const TitleArea = (): JSX.Element => {
         w={{ lg: '30%' }}
         zIndex={100}
         bg={bg}
-        // bgGradient="linear(to-br, gray.800, #e87d2c)"
         border='solid 1px transparent'
+        transform={`skew(-${skew}deg)`}
+        transformOrigin={'top'}
+        boxShadow={'dark-lg'}
       >
-        <Icon
-          display={{ base: 'none', lg: 'block' }}
-          position='absolute'
-          right={0}
-          top={-1}
-          bottom={0}
-          h='full'
-          w={40}
-          // color="#e87d2c"
-          color={bg}
-          transform='translateX(50.5%)'
-          fill='currentColor'
-          filter='drop-shadow(5px 0 5px)'
-          viewBox='0 0 100 100'
-          preserveAspectRatio='none'
-          aria-hidden='true'
-          zIndex={99}
-        >
-          <polygon points='50,0 100,0 50,100 0,100' />
-        </Icon>
         <Box
           mx='auto'
           maxW={{
@@ -46,6 +29,8 @@ const TitleArea = (): JSX.Element => {
           }}
           px={{ base: 4, sm: 6, lg: 8 }}
           mt={{ base: 10, sm: 12, md: 16, lg: 20, xl: 28 }}
+          transform={`skew(${skew}deg)`}
+          transformOrigin={'top'}
         >
           <VStack spacing='100%'>
             <Box
