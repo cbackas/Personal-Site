a3388590c8673a07e704f11fa29c813df1bcf55b
cbackas <zgibson@live.com>
2023-06-01 19:59:27 -0500

    add astro image deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import image from '@astrojs/image';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zacgibson.com'
+  site: 'https://zacgibson.com',
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
