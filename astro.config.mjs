2de282ff8789efeea09d44ff3b5f240a78748257
cbackas <zgibson@live.com>
2023-11-26 21:25:13 -0600

    update to astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-import image from '@astrojs/image';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zacgibson.com',
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  integrations: []
 });
